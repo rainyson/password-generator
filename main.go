33f8b0d94f5b4b82841ddf7491774fe054c46bbe
Babak Shokouhi Pour <b.shokouhipour@zeema.fund>
2025-11-22 12:58:43 +0000

    feat: implement a command-line password generator with user input for length and validation.

--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	fmt.Print("Enter Password Length: ")
+	length, err := getLengthInput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	password := generatePassword(length)
+	fmt.Println("Generated Password:", password)
+}
+
+func generatePassword(length int64) string {
+	passwordChars := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-={}[]:;,.<>?")
+	passwordCharsLength := len(passwordChars)
+	password := make([]rune, length)
+	for index := range length {
+		password[index] = passwordChars[rand.Intn(passwordCharsLength)]
+	}
+	return string(password)
+}
+
+func getLengthInput() (int64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	length, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	length = strings.ReplaceAll(length, "\n", "")
+	lengthNum, err := strconv.ParseInt(length, 0, 64)
+	if err != nil || lengthNum <= 0 {
+		return 0, fmt.Errorf("Your length: %s, is not a valid number\n", length)
+	}
+	return lengthNum, nil
 }
